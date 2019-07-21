89c5d56318abe031e51bd832b0c6277824ffa488
Ivan Lesar <ivan.lesar@degordian.com>
2019-07-21 17:25:16 +0200

    Add unity project

---
+++ unity/After5/Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        // Calculate movement velocity as a 3D vector
+        float _xMovement = Input.GetAxisRaw("Horizontal");
+        float _zMovement = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movementHorizontal = transform.right * _xMovement;
+        Vector3 _movementVertical = transform.forward * _zMovement;
+
+        // Final movement vector
+        Vector3 _velocity = (_movementHorizontal + _movementVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRotation, 0f, 0f) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+    }
+}
