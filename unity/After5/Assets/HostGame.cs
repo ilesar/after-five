724ee7a43a2a8463945322b4c759d1803104e78b
Ivan Lesar <ivan.lesar@degordian.com>
2020-03-21 20:31:42 +0100

    Matchmaking implemented

---
+++ unity/After5/Assets/HostGame.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+
+    [SerializeField]
+    private uint roomSize = 4;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom()
+    {
+        if (roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
